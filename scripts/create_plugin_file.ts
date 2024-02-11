336797974a3d972a9acea740069ca99844560c81
Casper Lindschouw <casper@lindschouw.me>
2024-02-11 14:24:03 +0100

    Fix generated plugin.json pointing to dprint org

--- scripts/create_plugin_file.ts
+++ scripts/create_plugin_file.ts
@@ -11,7 +11,48 @@ if (version == null || !/^\d+\.\d+\.\d+$/.test(version)) {
   throw new Error("Error extracting version.");
 }
 
-await processPlugin.createDprintOrgProcessPlugin({
+async function createProcessPlugin({ pluginName, version, platforms, isTest }: {
+  pluginName: string;
+  version: string;
+  platforms: processPlugin.Platform[];
+  /** Creates a plugin file with only the current platform using
+   * a zip file in the current folder.
+   */
+  isTest: boolean;
+}) {
+  const builder = new processPlugin.PluginFileBuilder({
+    name: pluginName,
+    version: version,
+  });
+
+  if (isTest) {
+    const platform = processPlugin.getCurrentPlatform();
+    const zipFileName = processPlugin.getStandardZipFileName(builder.pluginName, platform);
+    await builder.addPlatform({
+      platform,
+      zipFilePath: zipFileName,
+      zipUrl: zipFileName,
+    });
+  } else {
+    for (const platform of platforms) {
+      await addPlatform(platform);
+    }
+  }
+
+  await builder.writeToPath("plugin.json");
+
+  async function addPlatform(platform: processPlugin.Platform) {
+    const zipFileName = processPlugin.getStandardZipFileName(builder.pluginName, platform);
+    const zipUrl = `https://github.com/Phault/${pluginName}/releases/download/${builder.version}/${zipFileName}`;
+    await builder.addPlatform({
+      platform,
+      zipFilePath: zipFileName,
+      zipUrl,
+    });
+  }
+}
+
+await createProcessPlugin({
   pluginName: "dprint-plugin-csharpier",
   version,
   platforms: [
